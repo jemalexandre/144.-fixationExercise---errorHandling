dc45c23df4c328db9a3bcb5051ca496d053ec4be
jemalexandre <joseeduardo.alexandre@gmail.com>
2020-07-16 18:10:52 +0100

    Exercise solved. The proposed exception handling has been carried out.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,48 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+		//account opening
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double initialBalance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		//instance of account 
+		Account acc = new Account(number, name, initialBalance, withdrawLimit);
+		
+		//first withdrawal and balance update
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double withdraw = sc.nextDouble();
+		acc.withdraw(withdraw);
+		System.out.println(acc);
+		}
+		catch (DomainException e){
+			System.out.println(e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		
+		sc.close();
 	}
-
 }
